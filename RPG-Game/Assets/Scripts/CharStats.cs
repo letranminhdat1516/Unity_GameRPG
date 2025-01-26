17807322ff383963fe81e8808622e2e5eb0a98b1
Lê Trần Minh Đạt <94780550+letranminhdat1516@users.noreply.github.com>
2025-01-26 09:04:28 +0700

    player stats

---
+++ RPG-Game/Assets/Scripts/CharStats.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+    public string chartName;
+    public int playerLevel = 1;
+    public int playerEXP;   
+
+
+    public int currentHP;
+    public int maxHP=100;
+    public int currentMP;
+    public int maxMP=30;
+    public int strength;
+    public int defence;
+    public int wpnPwr;
+    public int armPwr;
+    public string equippedWpn;
+    public string equippedArmr;
+    public Sprite charImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
