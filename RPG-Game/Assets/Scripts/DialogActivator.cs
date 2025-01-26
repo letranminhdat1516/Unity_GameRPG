677c04dfaf672f7744c7c12c3318802857d869c2
Lê Trần Minh Đạt <94780550+letranminhdat1516@users.noreply.github.com>
2025-01-26 08:49:30 +0700

    Dialog box

---
+++ RPG-Game/Assets/Scripts/DialogActivator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogActivator : MonoBehaviour
+{
+    public string[] lines;
+    private bool canActivate;
+    public bool isPerson = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // show dialog box next line we set
+        if (canActivate && Input.GetButtonDown("Fire1") && !DialogManager.instance.dialogBox.activeInHierarchy)
+        {
+            DialogManager.instance.ShowDiaLog(lines,isPerson);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D orther)
+    {
+        if (orther.tag == "Player")
+        {
+            canActivate = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D orther)
+    {
+        if (orther.tag == "Player")
+        {
+            canActivate = false;
+        }
+    }
+}
--- RPG-Game/Assets/Scripts/PlayerController.cs
+++ RPG-Game/Assets/Scripts/PlayerController.cs
@@ -12,48 +12,64 @@ public class PlayerController : MonoBehaviour
     public string areaTransitionName;
     private Vector3 bottomLeftLimit;
     private Vector3 TopRightLimit;
-
+    
+    public bool canMove;
+    
     // Start is called before the first frame update
     void Start()
     {
-        if(instance == null)
+        canMove = true;
+        if (instance == null)
         {
             instance = this;
-        } else {
+        }
+        else
+        {
             if (instance != this)
             {
                 Destroy(gameObject);// Destroy xN player
             }
-            
+
         }
-        
+
         DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Setup moving with input setting "Horizontal" & "Vertical"
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
-
+      
+        if (canMove)
+        {
+            // Setup moving with input setting "Horizontal" & "Vertical"
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        }
+        else
+        {
+            theRB.velocity = Vector2.zero;
+        }
         // Set position moving wwith animator when u created 2 float in animator
-        myAnim.SetFloat("moveX",theRB.velocity.x);
-        myAnim.SetFloat("moveY",theRB.velocity.y);
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
 
         // Set position idle
-        if(Input.GetAxisRaw("Horizontal")==1|| Input.GetAxisRaw("Horizontal") ==-1||Input.GetAxisRaw("Vertical")==1|| Input.GetAxisRaw("Vertical") ==-1)
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
         {
-            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal")) ;
-            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical")) ;
+            if (canMove)
+            {
+                myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+                myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            }
+
         }
         // Control player in map 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x,bottomLeftLimit.x,TopRightLimit.x),
-        Mathf.Clamp(transform.position.y,bottomLeftLimit.y,TopRightLimit.y),
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, TopRightLimit.x),
+        Mathf.Clamp(transform.position.y, bottomLeftLimit.y, TopRightLimit.y),
         transform.position.z);
     }
     public void SetBounds(Vector3 botLeft, Vector3 topRight)
     {
-        bottomLeftLimit=botLeft + new Vector3(.5f,1f,0f);
-        TopRightLimit = topRight+ new Vector3(-.5f,-1f,0f);
+        bottomLeftLimit = botLeft + new Vector3(.5f, 1f, 0f);
+        TopRightLimit = topRight + new Vector3(-.5f, -1f, 0f);
     }
 }
